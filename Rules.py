e44049a13c7e37bc38ed7c62a4e067bb5cc3676a
christofer <christofer.rodriguez@ucr.ac.cr>
2019-04-26 22:17:24 -0600

    Inicio de la programacion de las reglas

--- Rules.py
+++ Rules.py
@@ -1,4 +1,8 @@
 import re
+from URLsReader import *
+from HTMLReader import *
+
+from unicodedata import normalize
 
 # Clase para implementar las reglas que se va a utilizar
 class Rules:
@@ -16,3 +20,44 @@ class Rules:
     def toLowerCase(self, stringDoc):
         return stringDoc.lower()
 
+    #Regla para quitar caracteres que no sean a-z, A-Z, 0-9, _
+    def removeCharactersInvalid(self, stringDoc):
+        return re.sub(r'[^\wa-zA-Z0-9_ ]', '', stringDoc)
+
+    #Regla para quitar tildes
+    def removeTick(self, stringDoc):
+        return normalize('NFC',
+                         re.sub(r"([^n\u0300-\u036f]|ñ(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+",
+                                r"\1",
+                                normalize("NFD", stringDoc),
+                                0,
+                                re.I)
+                         )
+
+    #Regla para eliminar palabras que son alfanumericas
+    def removeIfStartWithNumber(self, stringDoc):
+        #Hay que dejar ese espacio al final para que indique que es hasta que finalizo la palabra
+        return re.sub(r'[\d]+[a-zA-Z_]+? ', '', stringDoc)
+
+    #Regla para dejar los numeros hasta el 999,999
+    def removeNumberBiggerThan(self, stringDoc):
+        return re.sub(r'[\d]{7,}(,[\d]+)?', '', stringDoc)
+
+def main():
+    toy = URLsReader()
+
+    title = toy.getTitle()
+    #print(title)
+    html = HTMLReader(title)
+    html.printHtml()
+
+
+    rules = Rules()
+    doc = "55555 666666 7777777 333 4444 666666,99 999999,99999 1000000,00"
+    print(doc)
+    print("\n\n")
+    print(rules.removeNumberBiggerThan(doc))
+
+
+if __name__ == '__main__':
+    main()
