6aede15ef4b1d02b952888eadece7816cc9b3cbb
christofer <christofer.rodriguez@ucr.ac.cr>
2019-05-26 18:30:02 -0600

    Se agrega generador de wtd y se organiza en carpetas

--- FileGenerator.py
+++ FileGenerator.py
@@ -4,6 +4,7 @@ class FileGenerator:
     numberCharacteresFreqNorm = 20
     numberCharacteresNumberDocument = 12
     numberCharacteresFreqInv = 20
+    numberCharacteresWeight = 20
 
     # Funcion para rellenar espacios para que la hilera string quede de numberCharacteres de caracteres
     def formatNumberCharacters(self, numberCharacteres, string):
@@ -14,7 +15,7 @@ class FileGenerator:
     def formatNnumberToString(self, numberCharacteres, number):
         return self.formatNumberCharacters(numberCharacteres,str(number))
 
-    # Funcion que crea una linea del archivo
+    # Funcion que crea una linea del archivo .tok
     def lineTok(self, term, freq, freqNorm):
         return self.formatNumberCharacters(self.numberCharacteresTerm, term) + ' ' + self.formatNnumberToString(self.numberCharacteresFreq,freq) + ' ' + self.formatNnumberToString(self.numberCharacteresFreqNorm, freqNorm)+'\n'
 
@@ -25,12 +26,13 @@ class FileGenerator:
     # 1 espacio
     # 20 espacios para la frecuencia normalizada
     def tokGenerate(self, fileName, dictFreq, dictFreqNorm):
-        fileSave = './DocumentosProcesados/' + fileName + '.tok'
+        fileSave = './DocumentosProcesados/tok/' + fileName + '.tok'
         file = open(fileSave, 'w')
         for word in sorted(dictFreq.keys()):
             file.write(self.lineTok(word, dictFreq[word], dictFreqNorm[word]))
+        print(fileName + ".tok generado")
 
-    # Funcion que crea una linea del archivo
+    # Funcion que crea una linea del archivo vocabulario
     def lineVocabulary(self, term, numberDocument, freqInv):
         return self.formatNumberCharacters(self.numberCharacteresTerm, term) + ' ' + self.formatNnumberToString(
             self.numberCharacteresNumberDocument, numberDocument) + ' ' + self.formatNnumberToString(self.numberCharacteresFreqInv,
@@ -42,7 +44,24 @@ class FileGenerator:
     # 1 espacio en blanco
     # 20 caracteres para la frecuencia inversa
     def vocabularyGenerate(self, fileName, dictFreq):
-        fileSave = './DocumentosProcesados/' + fileName + '.txt'
+        fileSave = './DocumentosProcesados/vocabulario/' + fileName + '.txt'
         file = open(fileSave, 'w')
         for word in sorted(dictFreq.keys()):
             file.write(self.lineVocabulary(word, dictFreq[word].getNumDocs(), dictFreq[word].getFrecInversa()))
+        print("vocabulario.txt generado")
+
+    # Funcion que crea una linea del archivo .wtd
+    def lineWtd(self, term, peso):
+        return self.formatNumberCharacters(self.numberCharacteresTerm, term) + ' ' + self.formatNnumberToString(
+            self.numberCharacteresWeight, peso) + '\n'
+
+    # Metodo que genera el archivo .wtd
+    # 30 caracteres termino
+    # 1 espacio
+    # 20 espacios para el peso del termino en el documento
+    def vocabularyWtd(self, fileName, dictWeight):
+        fileSave = './DocumentosProcesados/wtd/' + fileName + '.wtd'
+        file = open(fileSave, 'w')
+        for word in sorted(dictWeight.keys()):
+            file.write(self.lineWtd(word, dictWeight[word]))
+        print(fileName + ".wtd generado")
